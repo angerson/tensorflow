2e26ee08914e7ff3754f67b2361e88f860cfdca7
Austin Anderson <angerson@google.com>
2019-09-12 13:27:37 -0700

    fixes Thu 12 Sep 2019 01:27:37 PM PDT

--- tensorflow/tools/ci_build/buildkite/packer/build_docker_image.sh
+++ tensorflow/tools/ci_build/buildkite/packer/build_docker_image.sh
@@ -4,7 +4,8 @@ set -euxo pipefail
 
 if git diff --name-only @~..@ | grep tensorflow/tools/ci_build/buildkite/packer/linux-container; then
   echo "+++ :packer: for :docker:"
-  packer build tensorflow/tools/ci_build/buildkite/packer/linux-container.json
+  cd tensorflow/tools/ci_build/buildkite/packer/
+  packer build linux-container.json
 else
   echo "Docker config not changed, no updates necessary"
 fi
--- tensorflow/tools/ci_build/buildkite/packer/build_linux_image.sh
+++ tensorflow/tools/ci_build/buildkite/packer/build_linux_image.sh
@@ -4,7 +4,8 @@ set -euxo pipefail
 
 if git diff --name-only @~..@ | grep tensorflow/tools/ci_build/buildkite/packer/linux-image; then
   echo "+++ :packer: for :docker:"
-  packer build tensorflow/tools/ci_build/buildkite/packer/linux-image.json
+  cd tensorflow/tools/ci_build/buildkite/packer/
+  packer build linux-image.json
 else
   echo "Image config not changed, no updates necessary"
 fi
