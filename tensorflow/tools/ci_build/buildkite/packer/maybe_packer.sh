3f53a91d18a4be0ae9c3f669ab8d3c155e17ca99
Austin Anderson <angerson@google.com>
2019-09-20 13:54:33 -0700

    fixes Fri 20 Sep 2019 01:54:33 PM PDT

--- tensorflow/tools/ci_build/buildkite/packer/maybe_packer.sh
+++ tensorflow/tools/ci_build/buildkite/packer/maybe_packer.sh
@@ -7,6 +7,7 @@ PACKER_TARGET=$1
 echo "$GCP_PROJECT_ID"
 echo "$ANOTHER_ENV_PARAM"
 echo "$BUILDKITE_AGENT_TOKEN" | cut -c1-5
+echo "$BTOK" | cut -c1-5
 
 if git diff --name-only @~..@ | grep tensorflow/tools/ci_build/buildkite/packer/$PACKER_TARGET; then
   echo "+++ :packer:"
